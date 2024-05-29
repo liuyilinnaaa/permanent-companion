82966bbb949bcf28edc9fe7f2c7957424287a975
liuyilinnaaa <liuyilinnaaa@gmail.com>
2024-05-29 23:43:36 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('acceptButton').addEventListener('click', function() {
+    alert('感谢你的接受！');
+});
+
+document.getElementById('declineButton').addEventListener('click', function() {
+    alert('很遗憾你拒绝了。');
+});
